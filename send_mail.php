5fcd0dc8c72560002066b9d74d9f29dfedce6bb3
octaruggieri <oruggieri@ipem206fernandofader.edu.ar>
2024-01-14 16:25:52 -0300

    commite temporal

--- send_mail.php
+++ send_mail.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://1-xactime-water.netlify.app");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json");
