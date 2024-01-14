881fc1e8c293a3aaf1927a3e48b372f951d06739
octaruggieri <oruggieri@ipem206fernandofader.edu.ar>
2024-01-14 16:10:36 -0300

    commite temporal

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json");
 
 // Incluir el autoload de Composer
 require 'vendor/autoload.php';
